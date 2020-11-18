a0242cd414fadd1174987cfcd8f9c46e481db9ba
David Santiago <david@davidsantiago.net>
2020-11-18 09:19:23 +0100

    Parece que ahora no cachea la llamada a la api

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,3 @@
-self.addEventListener('install', function(e) {
-    console.log('[Service Worker] Install');
-});
-
 var cacheName = 'v1';
 var appShellFiles = [
     './',
@@ -14,21 +10,6 @@ self.addEventListener('install', function(e) {
         caches.open(cacheName).then(function(cache) {
             console.log('[Service Worker] Caching all: app shell and content');
             return cache.addAll(appShellFiles);
-        })
-    );
-});
-
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(r) {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-            return r || fetch(e.request).then(function(response) {
-                return caches.open(cacheName).then(function(cache) {
-                    console.log('[Service Worker] Caching new resource: '+e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
         })
     );
 });
